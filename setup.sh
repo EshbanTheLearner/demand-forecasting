bff4e886cf9fedd4f4ac29eed66357a593d48d4f
Eshban Suleman <eshban.suleman@TenX-pk.ai>
2023-04-10 10:56:57 +0500

    Initialized project

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+kaggle competitions download -c competitive-data-science-predict-future-sales
+
+unzip competitive-data-science-predict-future-sales
+
+mv competitive-data-science-predict-future-sales data
